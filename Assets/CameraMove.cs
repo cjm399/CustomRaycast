b61c1f7e8f0b570c2e737fae5f9e7a2ff8dc6f0d
Chris McCole <chrismccole1@gmail.com>
2021-05-21 15:02:57 -0400

    Start to optimize raycasting a little. Trying to draw meshes as if they were all different meshes. Drawing is terrible, but raycasting is getting better.

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float ogCamSpeed = .1f;
+    public float fastCamSpeed = 1f;
+
+    private float currCamSpeed;
+    private bool isFast = false;
+    void Update()
+    {
+        Vector3 moveDir = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveDir += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDir += -transform.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDir += -transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDir += transform.right;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            moveDir += transform.up;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            moveDir += -transform.up;
+        }
+        isFast = Input.GetKey(KeyCode.LeftShift);
+        currCamSpeed = (isFast ? fastCamSpeed : ogCamSpeed);
+        moveDir = moveDir.normalized * currCamSpeed;
+        transform.position += moveDir;
+    }
+}
--- Assets/game.cs
+++ Assets/game.cs
@@ -40,6 +40,12 @@ public struct entity
     public bounds bounds;
 }
 
+public struct render_obj
+{
+    public Mesh mesh;
+    public Material mat;
+}
+
 public struct world
 {
     public NativeArray<entity> entities;
@@ -50,13 +56,18 @@ public struct world
     {
         maxEntities = _maxEntities;
         entityCount = 0;
-        entities = new NativeArray<entity>(maxEntities, Allocator.Persistent, NativeArrayOptions.ClearMemory);//new entity[maxEntities];
+        entities = new NativeArray<entity>(maxEntities, Allocator.Persistent, NativeArrayOptions.ClearMemory);
     }
 }
 
 public class game : MonoBehaviour
 {
     private world gameWorld;
+
+    public Mesh boxMesh;
+    public Material material;
+    public Camera mainCam;
+    public render_obj[] renderObjs;
 
     public static v3 up = new v3(0, 1, 0);
     public static v3 down = new v3(0, -1, 0);
@@ -86,42 +97,32 @@ public class game : MonoBehaviour
         public void Execute()
         {
             raycast_result hit = new raycast_result();
-            SlowRayCast(gameWorld, start, dir, maxDist, out hit);
+#if false
+            SlowRayCast(gameWorld, start, dir, maxDist, out hit); 
+#else
+            RayCast(gameWorld, start, dir, maxDist, out hit);
+#endif
             hitRes[0] = hit;
         }
     }
 
-    private void OnDestroy()
+    private void OnDisable()
     {
         gameWorld.entities.Dispose();
     }
 
     private void Start()
     {
-        gameWorld = new world(30_000);
-        //SpawnLotsOfCubes(ref gameWorld, 29_999);
+        mainCam = Camera.main;
+        gameWorld = new world(30000);
+        renderObjs = new render_obj[gameWorld.maxEntities];
+        SpawnLotsOfCubes(ref gameWorld, gameWorld.maxEntities);
 
-        entity curr = CreateCubePrimative(new v3(0, 0, 0));
-        AddEntityToWorld(ref gameWorld, ref curr);
-
-        entity curr2 = CreateCubePrimative(new v3(1, 0, 1));
-        AddEntityToWorld(ref gameWorld, ref curr2);
-
-        NativeArray<raycast_result> hitResults = new NativeArray<raycast_result>(1, Allocator.TempJob);
-
+        raycast_result hitResult;
         System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-        var runMyJob = new RaycastJob { gameWorld = gameWorld, start = new v3(0, 0, -50), dir = forward, maxDist = 10_000, hitRes = hitResults };
-        runMyJob.Run();
+        FastRaycast(ref gameWorld, new v3(-50, 0, -50), forward, 10000, out hitResult);
         sw.Stop();
-        raycast_result hitResult = runMyJob.hitRes[0];
-        hitResults.Dispose();
         Debug.Log($"Raycast took {sw.ElapsedMilliseconds}ms and hit {hitResult.hitPos.x}, {hitResult.hitPos.y}, {hitResult.hitPos.z}");
-        /*
-        sw = System.Diagnostics.Stopwatch.StartNew();
-        SlowRayCast(gameWorld, new v3(0, 20, 0), forward, 10_000, out hitResult);
-        sw.Stop();
-        Debug.Log($"Raycast 2 took {sw.ElapsedMilliseconds}ms");*/
-
 
 #if true
         RayCastAlongSphere(ref gameWorld, new v3(0, 0, 0), 20f);
@@ -135,6 +136,14 @@ public class game : MonoBehaviour
 #endif
     }
 
+    private void Update()
+    {
+        for (int i = 0; i < gameWorld.entityCount; ++i)
+        {
+            Graphics.DrawMesh(renderObjs[i].mesh, v3ToVector3(gameWorld.entities[i].position), Quaternion.identity, renderObjs[i].mat, 1, mainCam);
+        }
+    }
+
     private void SpawnLotsOfCubes(ref world _w, int _spawnCount)
     {
         int dimSizeX = (int)Mathf.Floor(Mathf.Sqrt(_spawnCount));
@@ -146,11 +155,15 @@ public class game : MonoBehaviour
         {
             for (int x = 0; x < dimSizeX; ++x)
             {
+                render_obj ro = new render_obj();
+                ro.mesh = boxMesh;
+                ro.mat = material;
                 entity curr = CreateCubePrimative(new v3(x + paddingX, y + paddingY, 0));
-                AddEntityToWorld(ref _w, ref curr);
+                AddEntityToWorld(ref _w, ref curr, ref renderObjs, ref ro);
                 paddingX += .25f;
             }
             paddingY += .25f;
+            paddingX = .5f;
         }
     }
 
@@ -269,6 +282,15 @@ public class game : MonoBehaviour
             && (_b.minPoints.x < _pos.x && _b.minPoints.y < _pos.y && _b.minPoints.z < _pos.z);
     }
 
+    [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    [System.Diagnostics.Contracts.Pure]
+    public static bool AABBOverlap(bounds _a, bounds _b)
+    {
+        return (_a.minPoints.x <= _b.maxPoints.x && _a.maxPoints.x >= _b.minPoints.x)
+            && (_a.minPoints.y <= _b.maxPoints.y && _a.maxPoints.y >= _b.minPoints.y)
+            && (_a.minPoints.z <= _b.maxPoints.z && _a.maxPoints.z >= _b.minPoints.z);
+    }
+
 
     [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
     [System.Diagnostics.Contracts.Pure]
@@ -366,11 +388,12 @@ public class game : MonoBehaviour
     [System.Diagnostics.Contracts.Pure]
     public static bool SlowRayCast(world _world, v3 _start, v3 _dir, float _maxDist, out raycast_result _hitResult)
     {
-        float stepDelta = .1f;
-        float currStep = stepDelta;
-        v3 checkPos = _start;
         v3 finalPos = v3FromDirAndMag(_start, _dir, _maxDist);
         _hitResult = new raycast_result();
+
+        v3 checkPos = _start;
+        float stepDelta = .1f;
+        float currStep = stepDelta;
         while (currStep < _maxDist)
         {
             for(int entityIndex = 0;
@@ -391,7 +414,90 @@ public class game : MonoBehaviour
         return false;
     }
 
-    public static void AddEntityToWorld(ref world _w, ref entity _e)
+    public static bool RayCast(world _world, v3 _start, v3 _dir, float _maxDist, out raycast_result _hitResult)
+    {
+        _hitResult = new raycast_result();
+        v3 finalPos = v3FromDirAndMag(_start, _dir, _maxDist);
+        bounds raycastAABB = new bounds();
+
+        raycastAABB.maxPoints.x = finalPos.x;
+        raycastAABB.minPoints.x = _start.x;
+        raycastAABB.maxPoints.y = finalPos.y;
+        raycastAABB.minPoints.y = _start.y;
+        raycastAABB.maxPoints.z = finalPos.z;
+        raycastAABB.minPoints.z = _start.z;
+
+        if (_start.x > finalPos.x)
+        {
+            raycastAABB.maxPoints.x = _start.x;
+            raycastAABB.minPoints.x = finalPos.x;
+        }
+        if (_start.y > finalPos.y)
+        {
+            raycastAABB.maxPoints.y = _start.y;
+            raycastAABB.minPoints.y = finalPos.y;
+        }
+        if (_start.z > finalPos.z)
+        {
+            raycastAABB.maxPoints.z = _start.z;
+            raycastAABB.minPoints.z = finalPos.z;
+        }
+
+        float closestSqrDistance = 100000000;
+        int closestIndex = -1;
+        NativeArray<int> entitiesInsideBB = new NativeArray<int>(_world.entityCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+        //int[] entitiesInsideBB = new int[_world.entityCount];
+        int count = 0;
+        for (int entityIndex = 0;
+                entityIndex < _world.entityCount;
+                ++entityIndex)
+        {
+            if(AABBOverlap(raycastAABB, _world.entities[entityIndex].bounds))
+            {
+                v3 pos = _world.entities[entityIndex].position;
+                float a = _start.x - pos.x;
+                a *= a;
+                float b = _start.y - pos.y;
+                b *= b;
+                float c = _start.z - pos.z;
+                c *= c;
+                float sqrDist = a + b + c;
+                if(sqrDist < closestSqrDistance)
+                {
+                    closestSqrDistance = sqrDist;
+                    closestIndex = entityIndex;
+                    entitiesInsideBB[count++] = entityIndex;
+                }
+            }
+        }
+
+        v3 checkPos = v3FromDirAndMag(_start, _dir, Mathf.Sqrt(closestSqrDistance) - 1);
+        float stepDelta = .1f;
+        float currStep = stepDelta;
+        while (currStep < _maxDist)
+        {
+            for (int entityIndex = 0;
+                entityIndex < count;
+                ++entityIndex)
+            {
+                int realIndex = entitiesInsideBB[entityIndex];
+
+                if (IsInside(_world.entities[realIndex].bounds, checkPos))
+                {
+                    _hitResult.hitEntity = _world.entities[realIndex];
+                    _hitResult.hitPos = checkPos;
+                    _hitResult.didHit = true;
+                    return true;
+                }
+            }
+            checkPos = v3FromDirAndMag(_start, _dir, currStep);
+            currStep += stepDelta;
+        }
+        entitiesInsideBB.Dispose();
+        return false;
+    }
+
+    public static void AddEntityToWorld(ref world _w, ref entity _e, ref render_obj[] _ros, ref render_obj _ro)
     {
         if(_w.entityCount -1 == _w.maxEntities)
         {
@@ -402,7 +508,10 @@ public class game : MonoBehaviour
             _w.entities = new NativeArray<entity>(_w.maxEntities, Allocator.Persistent, NativeArrayOptions.ClearMemory);
             NativeArray<entity>.Copy(tmp, _w.entities);
             tmp.Dispose();
+
+            System.Array.Resize<render_obj>(ref _ros, _w.maxEntities);
         }
+        _ros[_w.entityCount] = _ro;
         _w.entities[_w.entityCount++] = _e;
     }
 
